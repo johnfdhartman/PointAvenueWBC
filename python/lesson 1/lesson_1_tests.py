9133bea158a86af8f2f3763c0841061369f0fa19
John Hartman <john.f.d.hartman@gmail.com>
2019-10-29 16:48:37 +0700

    fixed some syntax errors

--- python/lesson 1/lesson_1_tests.py
+++ python/lesson 1/lesson_1_tests.py
@@ -3,7 +3,8 @@ from test_functions import test_func
 
 sum_list_data = [
     {
-    input: [1,2,3,4],
-    target: 10
-    exp: 'can sum a normal list'
+    'in_data': [1,2,3,4],
+    'target': 10,
+    'exp': 'can sum a normal list'
     }
+]
--- python/lesson 1/test_functions.py
+++ python/lesson 1/test_functions.py
@@ -1,3 +1,5 @@
 def test_func (func, test_data):
     print('testing ' + func.__name__)
-    
+    for test in test_data:
+        if func(test['in_data']) == test['target']:
+            print ('passed: ' + test['exp'])
