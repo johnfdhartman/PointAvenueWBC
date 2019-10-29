b1787b17120d873d828373a6ee8af59070df5fab
John Hartman <john.f.d.hartman@gmail.com>
2019-10-29 18:04:00 +0700

    test_func handles multiple parameter functions

--- python/lesson 1/test_functions.py
+++ python/lesson 1/test_functions.py
@@ -9,9 +9,15 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def test_func (func, test_data):
+    num_args = len(func.func_code.co_varnames)
+
     print('testing ' + func.__name__)
     for test in test_data:
-        if func(test['in_data']) == test['target']:
+        if num_args == 1:
+            result = func(['in_data'])
+        else if num_args > 1:
+            result = func(*['in_data'])
+        if result == test['target']:
             print (bcolors.OKBLUE + 'passed: ' + test['exp']+ bcolors.ENDC)
         else:
             print (bcolors.FAIL + 'failed: ' + test['exp']+ bcolors.ENDC)
