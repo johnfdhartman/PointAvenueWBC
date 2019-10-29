bb5cec98ba73bbee20f8300510d45a0dfa1c748a
John Hartman <john.f.d.hartman@gmail.com>
2019-10-29 16:54:55 +0700

    fixed test function, added text colouring

--- python/lesson 1/lesson_1_tests.py
+++ python/lesson 1/lesson_1_tests.py
@@ -1,10 +1,25 @@
-from lesson_1_exercises import sum_list
+from lesson_1_answers import sum_list
 from test_functions import test_func
 
 sum_list_data = [
     {
     'in_data': [1,2,3,4],
     'target': 10,
-    'exp': 'can sum a normal list'
-    }
+    'exp': 'should sum a list of positive integers'
+    },
+    {
+    'in_data': [],
+    'target': 0,
+    'exp': 'should accept lists with no elements'
+    },
+    {
+    'in_data': [5],
+    'target': 5,
+    'exp': 'should accept lists with 1 element'
+    },
+    {
+    'in_data': [-5,4,5,-6],
+    'target': -2,
+    'exp': 'should accept lists with negative elements'
+    },
 ]
--- python/lesson 1/test_functions.py
+++ python/lesson 1/test_functions.py
@@ -1,5 +1,17 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def test_func (func, test_data):
     print('testing ' + func.__name__)
     for test in test_data:
         if func(test['in_data']) == test['target']:
-            print ('passed: ' + test['exp'])
+            print (bcolors.OKBLUE + 'passed: ' + test['exp']+ bcolors.ENDC)
+        else:
+            print (bcolors.FAIL + 'failed: ' + test['exp']+ bcolors.ENDC)
