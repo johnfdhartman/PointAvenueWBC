d784e6ff7e847becf6335c64f9d5a54dda9c94dc
John Hartman <john.f.d.hartman@gmail.com>
2019-10-30 17:36:31 +0700

    added some exercises for lesson 2

--- python/lesson_2_exercises.py
+++ python/lesson_2_exercises.py
@@ -13,7 +13,21 @@ def bubble_sort (unsorted):
     return None
 
 #Bubble sort is famous for being really inefficient and slow.
-#Can you think of another way of sorting a list that might be faster?
+#Can you think of another way of sorting a list of numbers that might be faster?
 
 def my_sort (unsorted):
+    return None
+
+#A nested list is a list that contains lists.
+#E.g the list [ [1,2,3],[4,5,6],[7,8,9] ] has 3 elements, each of which
+#is also a list.
+#lists can also be deeply nested - which means that lists can contain
+#lists which can contain lists, and so on and so forth.
+
+#Write a function that 'flattens' a list.
+#That is, it takes a deeply nested list and returns a new list with all
+#the non-list elements *in the correct order*.
+#for example: [ [[1],2], [[3]], [4], 5] -> [1,2,3,4,5]
+
+def flatten_list (deep_nested):
     return None
